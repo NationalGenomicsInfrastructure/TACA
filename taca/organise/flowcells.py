ceb8266bf4f2655bd42971c49b5ec8cc4223e531
Sara Sjunnebo <sara.sjunnebo@gmail.com>
2025-11-26 15:44:41 +0100

    Ruff

--- taca/organise/flowcells.py
+++ taca/organise/flowcells.py
@@ -63,11 +63,11 @@ class NanoporeFlowcell(Flowcell):
         with filesystem.chdir(self.incoming_path):
             with open(tar_err, "w") as error_file:
                 tar_command = [
-                      "tar",
-                      *(["--exclude=pod5*"] if not self.include_pod5 else []),
-                      "-cvf",
-                      self.tar_path,
-                      self.fc_id,
+                    "tar",
+                    *(["--exclude=pod5*"] if not self.include_pod5 else []),
+                    "-cvf",
+                    self.tar_path,
+                    self.fc_id,
                 ]
                 result = subprocess.run(tar_command, stderr=error_file)
                 if result.returncode != 0:
